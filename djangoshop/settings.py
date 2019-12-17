e458cf4344bd1d6c67efe8f5c3385d300cadd847
Seyzar <57959846+Seyzar@users.noreply.github.com>
2019-12-17 16:40:50 +0300

    Update settings.py

--- djangoshop/settings.py
+++ djangoshop/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = '^+$kuy6ppwzufx_m1xf*$v70=xts@emnx5_*ys=g8fy_6hg%g$'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '^+$kuy6ppwzufx_m1xf*$v70=xts@emnx5_*ys=g8fy_6hg%g$')
+SECRET_KEY ='^+$kuy6ppwzufx_m1xf*$v70=xts@emnx5_*ys=g8fy_6hg%g$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 # DEBUG = bool( os.environ.get('DJANGO_DEBUG', False) )
